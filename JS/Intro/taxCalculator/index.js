e11cf0f0e13c2c442ecdcfa13b4ad1a07bc16ce6
Arthur-codeX <daniel.karanja239@gmail.com>
2026-06-29 09:24:06 +0300

    started functions

--- JS/Intro/taxCalculator/index.js
+++ JS/Intro/taxCalculator/index.js
@@ -1,14 +1,16 @@
-/*
- promt("")
+// /*
+//  promt("")
 
- checking if is a number
- 1.isNaN
- 2.Number->
- 3.+<>
+//  checking if is a number
+//  1.isNaN
+//  2.Number->
+//  3.+<>
 
-*/
+// */
 
 let grossMonthlySalary; //
+const personalRelief = 2400;
+
 while (true) {
   let gross = prompt("Enter your gross salary");
   if (isNaN(gross) === false) {
@@ -30,33 +32,92 @@ console.log(
 //->here
 //Calculate paye.
 
+let nssftier = null;
+
+let nssf = 0;
+
+if (grossMonthlySalary <= 9000) {
+  //tier 1
+  nssf = grossMonthlySalary * 0.06; // erning below 900;
+  console.log("nssf ", nssf);
+  nssftier = "nssf tier 1 below 9,000 KSH";
+} else if (grossMonthlySalary <= 108000) {
+  //tier 1 and tier2
+  let rem = grossMonthlySalary - 9000;
+  let tier1 = 9000 * 0.06;
+  let tier2 = rem * 0.06;
+  nssf = tier1 + tier2;
+  nssftier = "nssf tier 2 between 9,000 KSH - 108,000";
+} else {
+  nssf = 6840;
+  nssftier = "nssf tier 3 above 108,0000";
+}
+
+// console.log("nssf contibution", nssf);
+// alert(`nssf if ${nssf} and tier level is ${nssftier}`);
+
+const shif = grossMonthlySalary * (2.75 / 100);
+const housing = grossMonthlySalary * (1.5 / 100);
+
+let taxableIncome = grossMonthlySalary - (nssf + shif + housing);
+let paye_tier = null;
 let paye = null;
-let tier = null;
-if (grossMonthlySalary < 24000) {
-  paye = grossMonthlySalary * (10 / 100);
-  tier = "0-24,000 KES";
+
+const bracket1 = 24000;
+const bracket2 = 32333;
+const bracket3 = 500000;
+const bracket4 = 800000;
+
+const band1 = bracket1 * 0.1;
+const band2 = (bracket2 - bracket1) * 0.25;
+const band3 = (bracket3 - bracket2) * 0.3;
+const band4 = (bracket4 - bracket3) * 0.325;
+
+const personal_relief = 2400;
+
+if (taxableIncome <= 24000) {
+  paye = taxableIncome * 0.1;
+  paye_tier = "Paye 0-24000 KES";
+} else if (taxableIncome <= 32333) {
+  let diff = taxableIncome - 24000;
+  let tax = diff * 0.25;
+  paye = tax + band1;
+  paye_tier = "Paye 24000-32333 KES";
+} else if (taxableIncome <= 500000) {
+  let diff = taxableIncome - 32333;
+  let tax = diff * 0.3;
+  paye = tax + band1 + band2;
+  paye_tier = "Paye 32333-500000 KES";
+} else if (taxableIncome <= 800000) {
+  let diff = taxableIncome - 500000;
+  let tax = diff * 0.325;
+  paye = tax + band1 + band2 + band3;
+  paye_tier = "Paye 500000-800000 KES";
+} else {
+  let diff = taxableIncome - 800000;
+  let tax = diff * 0.35;
+  paye = tax + band1 + band2 + band3 + band4;
+  paye_tier = "Paye 800000 to infinity KES";
 }
 
-if (grossMonthlySalary > 24000 && grossMonthlySalary <= 32333) {
-  paye = grossMonthlySalary * (25 / 100);
-  tier = "24,000-32,333 KES";
-}
+const final_paye = Math.max(0, paye - personal_relief);
 
-if (grossMonthlySalary > 32333 && grossMonthlySalary <= 500000) {
-  paye = grossMonthlySalary * (30 / 100);
-  tier = "32,333-500,000 KES";
-}
+// alert(`Taxable Income ${taxableIncome}
+//     Paye ${paye}
+//     Personal Relief ${personalRelief}
+//     Final Paye ${final_paye}
+//     Tier ${paye_tier}
+//   `);
 
-if (grossMonthlySalary > 500000 && grossMonthlySalary <= 800000) {
-  //tire 3
-  paye = grossMonthlySalary * (32.5 / 100);
-  tier = "500,000-800,000 KES";
-}
+const totalDeduction = final_paye + nssf + shif + housing;
+const netSalary = grossMonthlySalary - totalDeduction;
 
-if (grossMonthlySalary > 800000) {
-  paye = grossMonthlySalary * (35 / 100);
-  tier = "800,000-infinity KES";
-}
-
-//->
-alert(`Paye is ${paye} and tier level is ${tier}`);
+alert(`For gross salary ${grossMonthlySalary}
+    nssf ${nssf} tier ${nssftier}
+    taxable Income ${taxableIncome}
+    paye ${paye} tier ${paye_tier}
+    shif ${shif}
+    housing ${housing}
+    totalDeduction ${totalDeduction}
+    Take Away ${netSalary}
+  `);
