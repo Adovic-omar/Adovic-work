b8c60dbfdd261777fd34f170e21cf9b85c9ce901
adovic-omar <adovicomar11@gmail.com>
2026-07-14 14:32:27 +0300

    updating my work

--- JS/frontEnd/domAccess.js
+++ JS/frontEnd/domAccess.js
@@ -42,3 +42,55 @@ console. log(queryList0fAllItems);
 for (let i = 0; i < queryList0fAllItems.length; i++) {
 console. log (querylist0fAllItems[i]);
 }
+
+/* buttons.
+1. original Content
+2. updating Content
+3. update the innerText
+*/
+function original () {
+console. 10g("original clicked");
+}
+function replace () {
+console. log("replace clicked");
+}
+function updateFruits () {
+console. log("update fruites),}
+}
+/*
+Update dom elements.
+ -innerHtml<replaces the html>
+ -innerText<replaces the innerText>
+1. buttons.
+1. original Content
+2. updating Content
+3. update the innerText
+*/
+ const otherFruits = ["Kiwi", "Pinapple", "Dragon Fruit"];
+ const originalDiv = document. querySelector("#div1") . innerHTML;
+//
+ // console. log(originalDiv);
+function original() {
+
+console.log("original clicked");
+ //reset the div to its original value
+document. querySelector("#div1"). innerHTML = originalDiv;
+ //replace the original content
+function replace() {
+// console. log( "replace clicked");
+const newHtml =
+<h3>List Chores</h3>
+<ul>
+‹li class="list-item">Clean Bathroom</li>
+‹li class="list-item"> Clean Kitchen</li>
+‹li class="list-item">Fetch Water</li>
+</ul>
+document. querySelector("#div1"). innerHTML = newHtm1;
+//some html
+function updateFruits() {
+console.log("update fruites clicked");
+
+ //innerText, for loop or while Loop.
+ //queryselectorAll. Only replace the fruijts using innertext
+
+//innnertText and loop<fruits>
