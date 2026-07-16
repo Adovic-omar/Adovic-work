6f3225950bdf21cd299423bf19e19040d0913a45
adovic-omar <adovicomar11@gmail.com>
2026-07-16 11:15:59 +0300

    updated my index.js

--- JS/frontEnd/github-project/index.js
+++ JS/frontEnd/github-project/index.js
@@ -15,3 +15,18 @@ fetch("https://api.github.com/users"
 console.1og("type of result", typeof result); console.1og(result);
 })
 •catch((error) = console error(error)) ;
+
+/*
+code without a promise.
+1. create a variable‹any name› global variable
+2. console.log the value of the variable.
+3. create and call a function that modifies this variable
+4. console.log() the value of the variable
+*/
+let flow = "This flow";
+console.1og("27:", flow);
+function testfunc () {
+flow = "updated flow";
+}
+testfunc ();
+console.1og("32:", flow);|
