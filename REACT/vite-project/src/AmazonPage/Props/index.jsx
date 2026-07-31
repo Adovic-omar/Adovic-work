3a1f9519a5657d73680795c060c9baf00d380d9b
adovic-omar <adovicomar11@gmail.com>
2026-07-31 12:47:34 +0300

    updated my work

--- REACT/vite-project/src/AmazonPage/Props/index.jsx
+++ REACT/vite-project/src/AmazonPage/Props/index.jsx
@@ -32,34 +32,102 @@ return (
 
 ) 
 }
-// display props using prop keyword function Child1(props) 
+// display props using prop keyword
 function Child1(props) {
-console.log (props);
-return (
-  <div>
-  <h1> I am the child 1 component</h1>
-  <ul>
-    <li>
-Favourite string <b>{props.str}</b>
-</li>
-    <li>
-Amount <b>{props.amountInNumber}</b>
-</li>
-    <li>
-isok <b>{props.is0k}</b>
-</li>
-    <li>
-isok <b>{String(props.isOk)}</b>{""}
-{/*
-for booleans convert them to string before displaying
-*/}
-</li>
-<li>
-    Not set <b>{props.notSet}</b>
-</li>
-  </ul>
-</div>
-   ),
+    return (
+        <div>
+            <h1> I am the child 1 component</h1>
+            <ul>
+                <li>
+                    Favourite string <b>{props.str}</b>
+                </li>
+                <li>
+                    Amount <b>{props.amountInNumber}</b>
+                </li>
+                <li>
+                    isok <b>{props.is0k}</b>
+                </li>
+                <li>
+                    isok <b>{String(props.isOk)}</b>{""}
+                    {/*
+                        for booleans convert them to string before displaying
+                    */}
+                </li>
+                <li>
+                    Not set <b>{props.notSet}</b>
+                </li>
+            </ul>
+        </div>
+    );
 }
+// using destructuring 
+function Child2(props) {
+    /*
+    key:value
+    const str-props.str
+    const amountInNumber-props.amountInNumber
+    const amount-props.amountInNumber
+   key:value<>
+   key:<string variable naming> "hi boss":34
+   const {str, amountInNumber, is0k, notSet} = props;
+    */
+    const { str, amountInNumber, is0k, notSet } = props;
+    return (
+        <div>
+            <h1> I am the child 2 component</h1>
+            <ul>
+                <li>
+                    Favourite string <b>{str}</b>
+                </li>
+                <li>
+                    Amount <b>{amountInNumber}</b>
+                </li>
+                <li>
+                    isok <b>{is0k}</b>
+                </li>
+                <li>
+                    isok <b>{String(is0k)}</b>{""}
+                    {/*
+                        for booleans convert them to string before displaying
+                    */}
+                </li>
+                <li>
+                    Not set <b>{notSet}</b>
+                </li>
+            </ul>
+        </div>
+    );
+}
+// display props using prop keyword
+// direct destructuring
+function Child3(props) {
+    //const { str, amountInNumber, is0k, notSet } = props;
+    return (
+        <div>
+            <h1> I am the child 3 component</h1>
+            <ul>
+                <li>
+                    Favourite string <b>{str}</b>
+                </li>
+                <li>
+                    Amount <b>{props.amountInNumber}</b>
+                </li>
+                <li>
+                    isok <b>{props.is0k}</b>
+                </li>
+                <li>
+                    isok <b>{String(props.isOk)}</b>{""}
+                    {/*
+                        for booleans convert them to string before displaying
+                    */}
+                </li>
+                <li>
+                    Not set <b>{props.notSet}</b>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
 
 export default Parent;
