2ec105d1c24e8ac3a2b56e8554e973ebce8dc417
adovic-omar <adovicomar11@gmail.com>
2026-08-05 11:02:44 +0300

    updated my work

--- REACT/vite-project/src/main.jsx
+++ REACT/vite-project/src/main.jsx
@@ -4,7 +4,7 @@ import {createRoot} from "react-dom/client";
 import ColorsAndCircles from "./ColorsAndCircles";"
 
 
-createRoot (document. getElementById("root")).render (
+createRoot (document. getElementById("root")).render(
 
   <StrictMode>
     <ColorsAndCircles />
