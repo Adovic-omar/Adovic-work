301a517d9adc5bc16066322d97d386f79dfa8b26
adovic-omar <adovicomar11@gmail.com>
2026-08-10 11:07:11 +0300

    learnt intro to react route

--- REACT/vite-project/src/main.jsx
+++ REACT/vite-project/src/main.jsx
@@ -1,12 +1,10 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import GithubProject from "./GithubProject";
+import Routing from "./Routing";
 
-
-createRoot (document. getElementById("root")).render(
-
-  <StrictMode>
-    <GithubProject />
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Routing />
+    </StrictMode>,
 );
