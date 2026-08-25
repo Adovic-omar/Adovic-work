7732c35a96c924162ee5941d776d8cab86fbf70f
adovic-omar <adovicomar11@gmail.com>
2026-08-25 10:32:34 +0300

    did intro to python

---
+++ PYTHON/intro/data_type.py
@@ -0,0 +1,19 @@
+# Example variables
+x = 10  # Integer
+y = 3.14  # Float
+z = "Hello"  # String
+a = True  # Boolean (True, False)
+b = [1, 2, 3, 4]  # List (Array, Mutable)
+c = {1, 2, 3}  # Set
+d = (1, 2, 3)  # Tuple (Immutable List)
+e = {"key": "value"}  # Dictionary (Object in JavaScript)
+
+# Determining the types
+print("X is ", x, "Its type ", type(x))  # Output: <class 'int'>
+print(f"Y is {y} its type is {type(y)}")  # Output: <class 'float'>
+print(type(z))  # Output: <class 'str'>
+print(type(a))  # Output: <class 'bool'>
+print(type(b))  # Output: <class 'list'>
+print(type(c))  # Output: <class 'set'>
+print(type(d))  # Output: <class 'tuple'>
+print(type(e))  # Output: <class 'dict'>
---
+++ PYTHON/intro/start.py
@@ -0,0 +1,9 @@
+# COMMENTS
+#console.log → print
+#let key →
+x=34
+print("x is =",x)
+y=20
+print("y is =",y)
+z=x+y
+print("z is = ",z)
